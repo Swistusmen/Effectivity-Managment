387b1228ccc82e21c0bf0f575c7cd6284f8908bf
Swistusmen <michal76aw@gmail.com>
2021-07-05 17:26:44 +0200

    updatedproject structure

--- app/src/main/java/com/example/efectivitymanagment/eis.kt
+++ app/src/main/java/com/example/efectivitymanagment/eis.kt
@@ -1,29 +1,24 @@
 package com.example.efectivitymanagment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class eis : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_eis)
 
-        val eisenhowerButton= findViewById<Button>(R.id.eisenhowerButton)
         val eisBackToMain=findViewById<Button>(R.id.eisBackToMain)
 
-        eisenhowerButton.setOnClickListener{
-            openEiseinhowerLayout()
+        eisBackToMain.setOnClickListener{
+            openMainLayout()
         }
-
-    }
-
-    private fun openEiseinhowerLayout() {
-        setContentView(R.layout.activity_eis)
     }
 
     private fun openMainLayout(){
+        Intent intent= new Intent(eis.this,MainActivity.class)
         setContentView(R.layout.activity_main)
     }
-
 }
