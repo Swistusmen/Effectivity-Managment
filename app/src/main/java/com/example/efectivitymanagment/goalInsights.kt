898dfad4ce8c4c8bb346ce9c987c351461e5b0ad
Swistusmen <michal76aw@gmail.com>
2021-07-17 14:25:16 +0200

    layout

---
+++ app/src/main/java/com/example/efectivitymanagment/goalInsights.kt
@@ -0,0 +1,11 @@
+package com.example.efectivitymanagment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class goalInsights : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_insights)
+    }
+}
