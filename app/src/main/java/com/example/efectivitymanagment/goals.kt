6ffe58dc907b972b985ad279f6a63bc63a4bd46b
Swistusmen <michal76aw@gmail.com>
2021-07-17 22:40:58 +0200

    layout extended

--- app/src/main/java/com/example/efectivitymanagment/goals.kt
+++ app/src/main/java/com/example/efectivitymanagment/goals.kt
@@ -1,11 +1,121 @@
 package com.example.efectivitymanagment
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import java.io.*
+import java.lang.StringBuilder
 
 class goals : AppCompatActivity() {
+    var Goals= arrayListOf<String>()
+    var fileName= "Goals"
+    var SQLDataBase="GoalsSteps"
+
+    var lineSeparator = System.getProperty("line.separator")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goals2)
+
+        var createButton=findViewById<Button>(R.id.goalsCreate)
+        var deleteButton=findViewById<Button>(R.id.goalsDelete)
+        var selectButton=findViewById<Button>(R.id.goalsEnter)
+        var list=findViewById<ListView>(R.id.goalsList)
+        var input=findViewById<EditText>(R.id.goalsTextEdit)
+
+        var adapter=ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,Goals)
+
+        createButton.setOnClickListener {
+            var nextText:String?=null
+            nextText=input.text.toString()
+            if(nextText!=null&&nextText!=""&&adapter!=null&&!CheckIfGoalExists(nextText)){
+                Goals.add(nextText)
+                list.adapter=adapter
+                adapter.notifyDataSetChanged()
+            }
+            input.text.clear()
+        }
+
+        selectButton.setOnClickListener {
+            if(adapter!=null&&list!=null){
+                var choosedAction: Int=list.checkedItemPosition
+                var GoalToOpen:String=Goals[choosedAction]
+                if(CheckIfGoalExists(GoalToOpen)){
+                var intent=Intent(this,goalInsights::class.java)
+                intent.putExtra("Goal",GoalToOpen)
+                startActivity(intent)
+                }
+            }
+        }
+
+        deleteButton.setOnClickListener {
+            //usuniecie wszystkiego z SQLa zwiazanego z tym taskiem
+            if(adapter!=null&&list!=null){
+                var choosenGoal:Int=list.checkedItemPosition
+                if(choosenGoal!=null) {
+                    adapter.remove(Goals.get(choosenGoal))
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        }
+
+        ReadGoals()
+        list.adapter=adapter
+        adapter.notifyDataSetChanged()
     }
+
+    override fun onPause() {
+        super.onPause()
+        SaveGoals()
+    }
+
+    fun ReadGoals(){
+        var path=this.getExternalFilesDir(null)
+        var file=File(path,fileName)
+        if(file.exists()){
+            var fileInput:FileInputStream?=null
+            fileInput=openFileInput(fileName)
+            var inputStreamReader:InputStreamReader= InputStreamReader(fileInput)
+            var bufferedReader:BufferedReader=BufferedReader(inputStreamReader)
+            var builder:StringBuilder=StringBuilder()
+            var newGoal:String?=null
+            while({newGoal=bufferedReader.readLine();newGoal}()!=null){
+                builder.append(newGoal)
+                Goals.add(builder.toString())
+                builder.clear()
+            }
+        }
+    }
+
+    fun SaveGoals(){
+        var path=this.getExternalFilesDir(null)
+        var file= File(path,fileName)
+        if(file.exists()){
+            file.delete()
+        }
+        file.createNewFile()
+        var fileOutput:FileOutputStream?=null
+        fileOutput=openFileOutput(fileName, Context.MODE_PRIVATE)
+        for(i in Goals){
+            fileOutput.write(i.toByteArray())
+            fileOutput.write(lineSeparator.toByteArray())
+        }
+        fileOutput.close()
+    }
+
+    fun CheckIfGoalExists(GoalTitle:String):Boolean{
+        for(i in Goals){
+            if (i==GoalTitle){
+                return true
+            }
+        }
+        return false
+    }
+
 }
